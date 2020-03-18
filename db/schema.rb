fc96505027798df44a57cbf7fea27681864eb7f8
nci <domodillon82@gmail.comgit config --global user.name Domo82git config --global domodillon82gmail.com>
2020-03-18 11:55:40 -0700

    added book/user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_124310) do
+ActiveRecord::Schema.define(version: 2020_03_18_182652) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_18_124310) do
     t.boolean "availability", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
