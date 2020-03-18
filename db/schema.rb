8ccb4fd881310483ae0e6b2b22307f3ad3576c17
nci <domodillon82@gmail.comgit config --global user.name Domo82git config --global domodillon82gmail.com>
2020-03-18 05:56:28 -0700

    added book scaffold and homepage logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_185150) do
+ActiveRecord::Schema.define(version: 2020_03_18_124310) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.text "description"
+    t.integer "price"
+    t.boolean "availability", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
