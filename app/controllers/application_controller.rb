fe0a0121cfdbf3946f437b1acda4861cdf4be7cc
nci <domodillon82@gmail.comgit config --global user.name Domo82git config --global domodillon82gmail.com>
2020-03-19 10:29:37 -0700

    added auth gem CanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, notice: "Sorry, You are not authorized to access the EDIT option"
+  end
 end
