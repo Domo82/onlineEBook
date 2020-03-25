b0d81e943a86c3c2ad422e4aebce9f1cf3dd9727
Domo82 <domodillon82@gmail.com>
2020-03-25 04:58:56 -0700

    added sales model

---
+++ app/models/sale.rb
@@ -0,0 +1,14 @@
+class Sale < ApplicationRecord
+  before_create :populate_uuid
+
+  belongs_to :book
+
+  validates_numericality of :price,
+  greater_than: 49, message: 'Price must be at least 50 cent'
+
+  private
+
+  def populate_uuid
+    self.uuid = SecureRandom.uuid()
+  end
+end
